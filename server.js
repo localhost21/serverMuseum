5e87938e06027e18a379583957823b3a7b1a3515
Alexander Horst <alexander.horst.ah@gmail.com>
2016-08-29 08:55:06 +0200

    3d commit

--- server.js
+++ server.js
@@ -306,7 +306,11 @@ app.get('/getBeacons', function(req, res){
 
 
 
-app.listen(3000);
-console.log("Server running on port 3000");
+//app.listen(3000);
+//console.log("Server running on port 3000");
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 //start server: node server
