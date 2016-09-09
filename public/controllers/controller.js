786d51cb665714163cd702d49b1bb07114024dbf
Alexander Horst <alexander.horst.ah@gmail.com>
2016-09-09 12:47:22 +0200

    latest

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -479,11 +479,11 @@ var refresh = function(){
       angular.forEach($scope.data, function(beacon) {
         $scope.i++;
 		if(beacon.color==="blueberry"){
-			beacon.img = "www/img/violet.png";
+			beacon.img = "https://www.dropbox.com/s/i1zbzaudgg90mpe/violet.png?dl=0";
 		}else if(beacon.color==="ice"){
-			beacon.img = "www/img/blue.png";
+			beacon.img = "https://www.dropbox.com/s/j0nohfh7easxsnj/blue.png?dl=0";
 		}else if(beacon.color==="mint"){
-			beacon.img = "www/img/mint.png";
+			beacon.img = "https://www.dropbox.com/s/q75k71vzwf9gdbu/mint.png?dl=0";
 		}
       });
     });
